c138c9ec9e6d6f611d4a80cfd7bc7b1cafc1af1b
Hodonk <115161072+Hodonk@users.noreply.github.com>
2022-12-25 11:18:01 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -20,8 +20,8 @@ let handler  = async (m, { conn, command, args, usedPrefix, DevMode }) => {
 //------------ BIO
 let ppown = await conn.profilePictureUrl(nomorown + '@s.whatsapp.net', 'image').catch(_ => hwaifu[1]) 
 let teksbio = `${htki} *BIODATA* ${htka}
-${htjava} *💌 Nama* : NexEp
-${htjava} *✉️ Nama RL* : Satria
+${htjava} *💌 Nama* : Fuad
+${htjava} *✉️ Nama RL* : Fuad
 ${htjava} *♂️ Gender* : Boys
 ${htjava} *🕋 Agama* : Islam
 ${htjava} *⏰ Tanggal lahir* : Private 🥶
@@ -29,8 +29,8 @@ ${htjava} *🎨 Umur* : PRIVATE
 ${htjava} *🧮 Kelas* : PRIVATE
 ${htjava} *🧩 Hobby* : MODIFIKASI WHATSAPP
 ${htjava} *💬 Sifat* : ASIK DAH KALAU DAH KENAL
-${htjava} *🗺️ Tinggal* : Jawa Barat, Bekasi
-${htjava} *❤️ Waifu* : Kaede Hitotsuba
+${htjava} *🗺️ Tinggal* : Jawa Timur, Gresik 
+${htjava} *❤️ Waifu* : -
 
 ${htjava} *📷 ɪɴsᴛᴀɢʀᴀᴍ* : ${sig}
 •·––––––––––––––––––––––––––·•
